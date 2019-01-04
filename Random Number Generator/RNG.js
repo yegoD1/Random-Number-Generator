4c848b1a24c9b1829c14f5fc902e04c56750c5fc
Hunor <hunrusz@gmail.com>
2019-01-03 21:20:26 -0500

    First upload of JS program.

---
+++ Random Number Generator/RNG.js
@@ -0,0 +1,33 @@
+var numOfRandomNumbers
+var numSize
+var numSet = []
+
+function calculateRandomNum(num1,num2) {
+    var i = 0
+    while(i<num1) {
+        numSet.push(randomIntFromInterval(1,num2))
+        i++
+    }
+
+}
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+window.alert('Random number generator.')
+numOfRandomNumbers = Number(window.prompt('How many random numbers do you need?'))
+while(Number.isInteger(numOfRandomNumbers) == false) {
+    window.alert('That is not a number.')
+    numOfRandomNumbers = Number(window.prompt('How many random numbers do you need?'))
+}
+numSize = Number(window.prompt("What's the max value do you want your number(s)?"))
+while(Number.isInteger(numSize) == false) {
+    window.alert('That is not a number.')
+    numSize = Number(window.prompt("What's the max value do you want your number(s)?"))
+}
+
+calculateRandomNum(numOfRandomNumbers,numSize)
+
+document.getElementById("Numbers").innerHTML = "These are your numbers,<br>" + numSet;
